696832a19e6e768c05fa6097a87112b93f039b48
44luvv <lvv44.procreator@gmail.com>
2026-06-13 21:00:23 +0300

    feat: добавлена интеграция с UptimeRobot и веб-сервер

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import asyncio
+import os
+from aiohttp import web
 from aiogram import Bot, Dispatcher
 from config import config
 from handlers.start import main_router
@@ -9,12 +11,28 @@ from databases.products import check_all_products
 
 dp = Dispatcher()
 
+async def handle_ping(request):
+    return web.Response(text="The Bot is working! Everything’s great!", status=200)
+
+
+async def start_web_server():
+    app = web.Application()
+    app.router.add_get("/", handle_ping)
+    port = int(os.getenv("PORT", 8080))
+
+    runner = web.AppRunner(app)
+    await runner.setup()
+    site = web.TCPSite(runner, "0.0.0.0", port)
+    await site.start()
+    print(f"Web-server started on port {port}")
+
+
 async def update():
     asyncio.create_task(check_all_products())
 
 async def main():
     bot = Bot(token=config.bot_token)
-
+    await start_web_server()
     dp.include_router(main_router)
     dp.include_router(menu_router)
     dp.include_router(cart_router)
@@ -30,6 +48,7 @@ async def main():
         except Exception as e:
             print(f"Failed to send startup message to admin: {e}")
 
+    print("🤖 Bot started!")
     await dp.start_polling(bot, db=db)
 
 
